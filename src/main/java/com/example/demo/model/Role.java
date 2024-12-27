864b5d95080d3a3f0a9e603847f5f78fd033a871
leucistic <leucist@yandex.by>
2024-12-27 15:39:23 +0300

    Changed DB naming pattern

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "Roles")
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
